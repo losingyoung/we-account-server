1efff26a6edfe8de73434d9f56bb1322504fd212
losingyoung <358925084@qq.com>
2018-03-30 10:17:09 +0800

    up

--- routers/group.js
+++ routers/group.js
@@ -7,26 +7,42 @@ router.post('/get_groups_by_wa_code', ctx => {
         groupInfos: [{
             id: 222,
             name: 'group1',
-            owner: 777,
+            owner_wa_code: 777,
+            ownerName:'losingyoung',
+            avatar: "http://ourrovucw.bkt.clouddn.com/avatar_girl.jpg",
             members: [{
                 wa_code: 333,
-                name: 'member1'
+                name: 'member1',
+                gender: "0",
+                avatar: "http://ourrovucw.bkt.clouddn.com/avatar_girl.jpg"
             }, {
                 wa_code: 777,
-                name: 'losingyoung'
+                name: 'losingyoung',
+                gender: "0",
+                avatar: "",
+                owner: true
             }] 
         }, {
             id: 223,
             name: 'group2',
-            owner: 777,
+            owner_wa_code: 737,
+            ownerName:'tiffany',
             members: [{
                 wa_code: 777,
                 name: 'losingyoung',
                 gender: "0",
                 avatar: ""
             }, {
+                wa_code: 737,
+                name: 'tiffany',
+                gender: "1",
+                avatar: "",
+                owner: true
+            },{
                 wa_code: 334,
-                name: 'member2'
+                name: 'member2',
+                gender: "0",
+                avatar: ""
             }] 
         }]
     }
