ff16892b3619a451effcb1d4d7faf80a125e86c9
losingyoung <358925084@qq.com>
2018-05-07 18:06:41 +0800

    update account

--- routers/account.js
+++ routers/account.js
@@ -1,10 +1,43 @@
 const Router = require('koa-router')
 const router = new Router()
+// const formidable = require("formidable");
+// const fs = require('fs')
+router.post("/add_item",async ctx => {
+            ctx.body = {
+            success: true
+        }
+  /*  console.log('file upload2', ctx)
 
-router.post("/add_item", ctx => {
-    ctx.body = {
-        success: true
-    }
+    var form = new formidable.IncomingForm();
+    // form.parse(ctx.req,async function(err,fields,files){
+    //     if(err){throw err; return;}
+    //     console.log('fields',fields);//{ name: base64字符串 }
+    //     ctx.body = {
+    //         success: true
+    //     }
+    // });
+    const targetPath = `${__dirname}/upload`;
+    // form.uploadDir = targetPath;
+    const files = [];
+    const fields = [];
+
+    form.on('file', (field, file) => { // 接收文件触发
+        console.log(`upload file: ${file.name}`);
+        // 判断文件类型是否是xlsx
+        // if (file.type !== 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet') {
+        //     fileTypeError = true;
+        // }
+        fs.writeFile(`upload/${file.name}`,file,(err) => {
+            console.log('end write')
+        })
+        files.push([field, file]);
+    });
+    form.on('end', () => {
+        let filess = files
+        console.log('end', files)
+    })
+    form.parse(ctx.req);
+*/
 })
 /*
  *wa_code
@@ -78,6 +111,55 @@ router.post("/get_group_account_items", ctx => {
     }
 })
 router.post('/get_account_items', ctx => {
+    const Details = [{
+        year: 2017,
+        totalCost: 40000,
+        monthDetails: [{
+                month: 11,
+                totalCost: 4000,
+                categoryDetail: [{
+                categoryId: 1,
+                categoryName: 'bus',
+                cost:432
+               }]
+            }, {
+                  month: 12,
+                  totalCost: 4200,
+                  categoryDetail: [{
+                    categoryId: 1,
+                    categoryName: 'bus',
+                    totalCost:432
+                 }]
+              }]
+       }, {
+            year: 2018,
+            totalCost: 4000,
+            monthDetails: [{
+                  month: 1,
+                  totalCost: 4000,
+                  categoryDetail: [{
+                    categoryId: 1,
+                    categoryName: 'bus',
+                    totalCost:432
+                 }]
+              }, {
+                    month: 2,
+                    totalCost: 4200,
+                    categoryDetail: [{
+                      categoryId: 1,
+                      categoryName: 'bus',
+                      totalCost:432
+                   }]
+                }]
+        }]
+        const monthData = Details.map(year => year.monthDetails.reduce((sum,month) => Object.assign(sum, {
+                    [month.month]: month
+                }), {})
+        ).reduce((sum, cur) =>  Object.assign(sum, cur), {})
+        console.log(monthData)
+        const yearData = Object.keys(monthData).map(month => {
+            return monthData[month]
+        })
     ctx.body={
         personalAccountItems: [{
             wa_code: 777,
@@ -85,23 +167,23 @@ router.post('/get_account_items', ctx => {
             averageDay: 50,
             chartData: {
                 totalOverview: {
-                    date: ["3","4","5", "6", "7", "8", "9", "10", "11", "12", "1", "2"],
+                    date: [3,4,5, 6, 7, 8, 9, 10, 11, 12, 1, 2],
                     data: [1234567,1,2,3,4,5,6,7,8,9,40,1220]
-                },
-                yearDetails: {
-                    2018: {
-                        monthDetails: {
-                           4: {
-                               totalCost: 4000,
-                               categoryDetail: [{
-                                categoryId: 1,
-                                categoryName: 'bus',
-                                cost:432
-                               }]
-                           }
-                        }
-                    }
                 }
+                // 根据yearDetails生成以下数据
+                /**每月 点击月 展示相应的category
+                 * {
+                 * 1:{category:[{}],person:[{}]}
+                 * 2:{category:[],person:[]}
+                 * }
+                 */
+
+                /**每年
+                 * {2017:{
+                 * category:[],
+                 * person:[]
+                 * }}
+                 */
             },
             items: [
                 {
@@ -112,7 +194,7 @@ router.post('/get_account_items', ctx => {
                     categoryName: 'bus',
                     CategoryType: 'coffee',
                     price: 20,
-                    description: 'some desc1',
+                    description: 'some desc1 some desc1 some desc1 some desc1some desc1some desc1some desc1some desc1some desc1',
                     date: 15201253,
                     year: 2018,
                     month: 4,
