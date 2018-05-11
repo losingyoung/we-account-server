2d17225e67ba53c9188d7cffdb5c0adf62b4de41
losingyoung <358925084@qq.com>
2018-05-11 13:20:29 +0800

    update account

--- routers/account.js
+++ routers/account.js
@@ -6,121 +6,102 @@ router.post("/add_item",async ctx => {
             ctx.body = {
             success: true
         }
-  /*  console.log('file upload2', ctx)
-
-    var form = new formidable.IncomingForm();
-    // form.parse(ctx.req,async function(err,fields,files){
-    //     if(err){throw err; return;}
-    //     console.log('fields',fields);//{ name: base64字符串 }
-    //     ctx.body = {
-    //         success: true
-    //     }
-    // });
-    const targetPath = `${__dirname}/upload`;
-    // form.uploadDir = targetPath;
-    const files = [];
-    const fields = [];
-
-    form.on('file', (field, file) => { // 接收文件触发
-        console.log(`upload file: ${file.name}`);
-        // 判断文件类型是否是xlsx
-        // if (file.type !== 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet') {
-        //     fileTypeError = true;
-        // }
-        fs.writeFile(`upload/${file.name}`,file,(err) => {
-            console.log('end write')
-        })
-        files.push([field, file]);
-    });
-    form.on('end', () => {
-        let filess = files
-        console.log('end', files)
-    })
-    form.parse(ctx.req);
-*/
 })
-/*
- *wa_code
- */
-router.post("/get_personal_account_items", ctx => {
-    ctx.body = {
-        totalCost: 1000,
-        averageDay: 50,
-        items: [
-            {
-                wa_code: 777,
-                categoryId: 1,
-                categoryName: 'bus',
-                CategoryType: 'coffee',
-                price: 20,
-                description: 'some desc',
-                date: 15201253,
-                dateStr:"2018年10月23日",
-                isOutcome: "0" //收入还是支出
-            },
-            {
-                wa_code: 777,
-                categoryId: 1,
-                categoryName: 'bus',
-                CategoryType: 'coffee',
-                price: 30,
-                description: 'some desc',
-                date: 152012503,
-                dateStr:"2018年10月23日",
-                isOutcome: "0" //收入还是支出
-            }
-        ]
-    }
-})
-/*
- *group_id
- */
-router.post("/get_group_account_items", ctx => {
-    ctx.body = {
-        totalCost: 1000,
-        averageDay: 50,
-        averageDayPerson: 30,
+// /*
+//  *wa_code
+//  */
+// router.post("/get_personal_account_items", ctx => {
+//     ctx.body = {
+//         totalCost: 1000,
+//         averageDay: 50,
+//         items: [
+//             {
+//                 wa_code: 777,
+//                 categoryId: 1,
+//                 categoryName: 'bus',
+//                 CategoryType: 'coffee',
+//                 price: 20,
+//                 description: 'some desc',
+//                 date: 15201253,
+//                 dateStr:"2018年10月23日",
+//                 isOutcome: "0" //收入还是支出
+//             },
+//             {
+//                 wa_code: 777,
+//                 categoryId: 1,
+//                 categoryName: 'bus',
+//                 CategoryType: 'coffee',
+//                 price: 30,
+//                 description: 'some desc',
+//                 date: 152012503,
+//                 dateStr:"2018年10月23日",
+//                 isOutcome: "0" //收入还是支出
+//             }
+//         ]
+//     }
+// })
+// /*
+//  *group_id
+//  */
+// router.post("/get_group_account_items", ctx => {
+//     ctx.body = {
+//         totalCost: 1000,
+//         averageDay: 50,
+//         averageDayPerson: 30,
 
-        items: [{
-            group_id:222,
-            wa_code: 777,
-            submitBy:777,
-            updateBy:888,
-            categoryId: 1,
-            categoryName: 'bus',
-            categoryType: 'coffee',
-            price: 20,
-            description: 'some desc',
-            date: 15201253,
-            dateStr:"2018年10月23日",
-            isOutcome: "0"
-        }, {
-            group_id:222,
-            wa_code: 777,
-            submitBy:777,
-            updateBy:888,
-            categoryId: 1,
-            categoryName: 'bus',
-            categoryType: 'coffee',
-            price: 20,
-            description: 'some desc',
-            date: 15201253,
-            dateStr:"2018年10月23日",
-            isOutcome: "0"
-        }]
-    }
-})
+//         items: [{
+//             group_id:222,
+//             wa_code: 777,
+//             submitBy:777,
+//             updateBy:888,
+//             categoryId: 1,
+//             categoryName: 'bus',
+//             categoryType: 'coffee',
+//             price: 20,
+//             description: 'some desc',
+//             date: 15201253,
+//             dateStr:"2018年10月23日",
+//             isOutcome: "0"
+//         }, {
+//             group_id:222,
+//             wa_code: 777,
+//             submitBy:777,
+//             updateBy:888,
+//             categoryId: 1,
+//             categoryName: 'bus',
+//             categoryType: 'coffee',
+//             price: 20,
+//             description: 'some desc',
+//             date: 15201253,
+//             dateStr:"2018年10月23日",
+//             isOutcome: "0"
+//         }]
+//     }
+// })
+Array.prototype.concatAll = function() {
+   return this.reduce(function(sum,cur) {
+        return sum.concat(cur)
+    }, [])
+}
+Array.prototype.reduceObj = function() {
+    return this.reduce((sum, cur) => Object.assign({}, sum, cur), {})
+}
 router.post('/get_account_items', ctx => {
     const Details = [{
         year: 2017,
         totalCost: 40000,
-        monthDetails: [{
+        monthDetail: [{
                 month: 11,
                 totalCost: 4000,
                 categoryDetail: [{
-                categoryId: 1,
-                categoryName: 'bus',
-                cost:432
+                    categoryId: 1,
+                    categoryName: 'bus',
+                    totalCost:432
+               }],
+               personDetail: [{
+                   wa_code:777,
+                   name: 'tiffan',
+                   totalCost:312
                }]
             }, {
                   month: 12,
@@ -134,11 +115,15 @@ router.post('/get_account_items', ctx => {
        }, {
             year: 2018,
             totalCost: 4000,
-            monthDetails: [{
+            monthDetail: [{
                   month: 1,
                   totalCost: 4000,
                   categoryDetail: [{
                     categoryId: 1,
+                    categoryName: 'bus',
+                    totalCost:432
+                 }, {
+                    categoryId: 2,
                     categoryName: 'bus',
                     totalCost:432
                  }]
@@ -146,21 +131,60 @@ router.post('/get_account_items', ctx => {
                     month: 2,
                     totalCost: 4200,
                     categoryDetail: [{
-                      categoryId: 1,
-                      categoryName: 'bus',
-                      totalCost:432
-                   }]
+                        categoryId: 1,
+                        categoryName: 'bus',
+                        totalCost:432
+                   }, {
+                        categoryId: 3,
+                        categoryName: 'bus',
+                        totalCost:432
+                 }]
                 }]
         }]
-        const monthData = Details.map(year => year.monthDetails.reduce((sum,month) => Object.assign(sum, {
-                    [month.month]: month
+        const monthData = Details.map(year => year.monthDetail.reduce((sum,month) => Object.assign(sum, {
+                    [month.month]: Object.assign(month, {
+                        person: month.personDetail && month.personDetail.reduce((sum, cur) => Object.assign({}, sum, {
+                            [cur.wa_code]: cur
+                        }), {}),
+                        category: month.categoryDetail && month.categoryDetail.reduce((sum, cur) => Object.assign({}, sum, {
+                            [cur.categoryId]: cur
+                        }), {})
+                    })
                 }), {})
         ).reduce((sum, cur) =>  Object.assign(sum, cur), {})
         console.log(monthData)
-        const yearData = Object.keys(monthData).map(month => {
-            return monthData[month]
-        })
-    ctx.body={
+        const yearData = Details.map(year => {
+           const catePersonTotal = year.monthDetail.map(month => {
+                return {categoryDetail: month.categoryDetail, personDetail: month.personDetail}
+            }).reduce((sum, cur) => {
+                if (cur.categoryDetail) {
+                    cur.categoryDetail.map(cate => {
+                        const curCateId = cate.categoryId
+                        if (typeof sum.category[curCateId] === 'undefined') {
+                            sum.category[curCateId] = Object.assign({}, cate, {totalCost: 0})
+                        }
+                        sum.category[curCateId].totalCost += cate.totalCost
+                    })
+                }
+                if (cur.personDetail) {
+                    cur.personDetail.map(person => {
+                        const curCateId = person.wa_code
+                        if (typeof sum.person[curCateId] === 'undefined') {
+                            sum.person[curCateId] = Object.assign({}, person, {totalCost: 0})
+                        }
+                        sum.person[curCateId].totalCost += person.totalCost
+                    })
+                }
+                return sum
+            }, {category: {}, person: {}})
+            return {
+                [year.year]:  Object.assign({
+                    totalCost: year.totalCost
+                }, catePersonTotal)
+            }
+        }).reduceObj()
+        console.log(yearData)
+       ctx.body={
         personalAccountItems: [{
             wa_code: 777,
             totalCost: 1000,
@@ -169,8 +193,10 @@ router.post('/get_account_items', ctx => {
                 totalOverview: {
                     date: [3,4,5, 6, 7, 8, 9, 10, 11, 12, 1, 2],
                     data: [1234567,1,2,3,4,5,6,7,8,9,40,1220]
-                }
-                // 根据yearDetails生成以下数据
+                },
+                yearData,
+                monthData
+                // 根据Details生成以下数据
                 /**每月 点击月 展示相应的category
                  * {
                  * 1:{category:[{}],person:[{}]}
@@ -180,8 +206,14 @@ router.post('/get_account_items', ctx => {
 
                 /**每年
                  * {2017:{
-                 * category:[],
-                 * person:[]
+                 *   category:[{
+                 *   categoryId:3123,
+                 *   totalCost:4444
+                 * }],
+                 * person:[{
+                 *   wa_code:777,
+                 *   totalCost:5000
+                 * }]
                  * }}
                  */
             },
